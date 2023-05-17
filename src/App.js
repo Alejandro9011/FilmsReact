a25a850a27f43c573a44f3efa2031b51eb7817da
Alex <alejandro.902019@hotmail.com>
2023-05-17 15:16:27 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,166 @@
+import { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
+import YouTube from "react-youtube";
+import { FaSearch } from "react-icons/fa";
+
+export function App() {
+  const API_URL = "https://api.themoviedb.org/3";
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+  const API_KEY = "1bdb1af1aa98a6724bc3854afafaaf5b";
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
+
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
+  const [playing, setPlaying] = useState(false);
+
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+    setMovies(results);
+    setMovie(results[0]);
+
+    if (results.length) {
+      await fetchMovie(results[0].id);
+    }
+  };
+
+  const fetchMovie = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "videos",
+      },
+    });
+
+    if (data.videos && data.videos.results) {
+      const trailer = data.videos.results.find(
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0]);
+    }
+
+    setMovie(data);
+  };
+  const selectMovie = async (movie) => {
+    fetchMovie(movie.id);
+    setMovie(movie);
+    window.scrollTo(0, 0);
+  };
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center-tittle mt-5 mb-5">Movies</h2>
+      <form className="containerForm mb-4" onSubmit={searchMovies}>
+        <div className="styleSearch">
+          <input
+            className="searchInput"
+            type="text"
+            placeholder="search"
+            onChange={(e) => setSearchKey(e.target.value)}
+          />
+          <button className="buttoninput">
+            <FaSearch />
+          </button>
+        </div>
+      </form>
+
+      <div className="All">
+        <main>
+          {movie ? (
+            <div
+              className="viewtrailer"
+              style={{
+                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+              }}
+            >
+              {playing ? (
+                <>
+                  <YouTube
+                    videoId={trailer.key}
+                    className="reproductor container"
+                    containerClassName={"youtube-container"}
+                    opts={{
+                      width: "100%",
+                      height: "100%",
+                      playerVars: {
+                        autoplay: 1,
+                        controls: 0,
+                        cc_load_policy: 0,
+                        fs: 0,
+                        iv_load_policy: 0,
+                        modestbranding: 0,
+                        rel: 0,
+                        showinfo: 0,
+                      },
+                    }}
+                  />
+                  <button
+                    onClick={() => setPlaying(false)}
+                    className="boton-close"
+                  >
+                    Close
+                  </button>
+                </>
+              ) : (
+                <div className="container">
+                  <div className="">
+                    {trailer ? (
+                      <button
+                        className="boton"
+                        onClick={() => setPlaying(true)}
+                        type="button"
+                      >
+                        Play Trailer
+                      </button>
+                    ) : (
+                      <h3 className="Sorry">"Sorry, no trailer available"</h3>
+                    )}
+                    <h1 className="text-white">{movie.title}</h1>
+                    <p className="text-white">{movie.overview}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : null}
+        </main>
+      </div>
+      <div className="container mt-3">
+        <div className="row">
+          {movies.map((movie) => (
+            <div
+              key={movie.id}
+              className="col-md-4 mb-3"
+              onClick={() => selectMovie(movie)}
+            >
+              <img
+                src={`${URL_IMAGE + movie.poster_path}`}
+                alt=""
+                height={600}
+                width="100%"
+              />
+              <h4 className="text-center">{movie.title}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
